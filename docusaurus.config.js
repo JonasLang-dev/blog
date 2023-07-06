1e7540dbe242b968a19004628100f1ec429e27ea
Jonas Lang <jonaslangdev@outlook.com>
2023-07-06 16:49:01 +0800

    fix: change the edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,15 +46,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/JonasLang-dev/blog/edit/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/JonasLang-dev/blog/blob/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),

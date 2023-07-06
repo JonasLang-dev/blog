3c5062123cb08e4400ad6dfa20ad0404bff45a85
Jonas Lang <jonaslangdev@outlook.com>
2023-07-06 17:34:01 +0800

    todo: pwa

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,6 +61,70 @@ const config = {
     ],
   ],
 
+  plugins: [
+    [
+      "pwa",
+      {
+        // debug: isDeployPreview,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString",
+        ],
+        // swRegister: false,
+        swCustom: require.resolve("./src/sw.js"),
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "img/docusaurus.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "manifest.json",
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-capable",
+            content: "yes",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-status-bar-style",
+            content: "#000",
+          },
+          {
+            tagName: "link",
+            rel: "apple-touch-icon",
+            href: "img/docusaurus.png",
+          },
+          {
+            tagName: "link",
+            rel: "mask-icon",
+            href: "img/docusaurus.png",
+            color: "rgb(62, 204, 94)",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileImage",
+            content: "img/docusaurus.png",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileColor",
+            content: "#000",
+          },
+        ],
+      },
+    ],
+  ],
+
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
@@ -99,8 +163,12 @@ const config = {
             title: "Documents",
             items: [
               {
-                label: "Tutorial",
-                to: "/docs/category/html",
+                label: "JavaScript",
+                to: "/docs/category/javascript",
+              },
+              {
+                label: "React",
+                to: "/docs/category/react",
               },
             ],
           },
